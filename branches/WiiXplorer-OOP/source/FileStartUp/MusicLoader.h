a8d08fd17142a0b3f0dd115dbb743c67ef74838d
e.bovendeur@gmail.com <e.bovendeur@gmail.com@1e5e7b04-5f56-11de-8abc-d1c337b90d21>
2009-09-26 10:53:23 +0000

    Creating a branch containing a redesigned (not gui, but architectural) version of WiiXplorer.

---
+++ branches/WiiXplorer-OOP/source/FileStartUp/MusicLoader.h
@@ -0,0 +1,6 @@
+#ifndef __MUSICLOADER_H
+#define __MUSICLOADER_H
+
+void LoadMusic(const char *filepath);
+
+#endif
---
+++ branches/WiiXplorer-OOP/source/tools.cpp
@@ -0,0 +1,22 @@
+#include <time.h>
+
+bool TimePassed(int limit)
+{
+	time_t timer1;
+	static time_t timer2;
+
+	if (timer2 == 0) {
+			time(&timer1);
+		    timer2 = timer1;
+    }
+
+    if(timer1 != 0)	// timer läuft
+        time(&timer1);
+
+    if(difftime(timer1, timer2) >= limit) {
+        timer1 = 0;
+        return true;
+    }
+
+    return false;
+}
