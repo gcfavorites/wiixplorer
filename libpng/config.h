354e0181d0570754952c8683b1f0d1bd79b8120e
dimok321 <dimok321@hotmail.de>
2010-11-04 20:36:51 +0000

    *Updated libpng to 1.4.4 *Updated libntfs to new NTFS3G 2010.10.02 *Added possibility to boot BootMii (in StartMenu) *Added possibility to boot movies with WiiMC (WiiMC is not parsing arguements yet. Rodries said he is going to add it eventually. WiiXplorer is ready for that :D.) *Removed "allowing of only available IOSes for BootIOS" feature. It backfired and some people couldn't change the IOS at all because of no NAND permission on the current running IOS. *Updated language files
    Forwarder Channel v9:
    - Fixed garbage on screen / green screen right before application boot

--- libpng/config.h
+++ libpng/config.h
@@ -4,46 +4,46 @@
 #undef HAVE_DLFCN_H
 
 /* Define to 1 if you have the <inttypes.h> header file. */
-#define HAVE_INTTYPES_H
+#define HAVE_INTTYPES_H 1
 
 /* Define to 1 if you have the `m' library (-lm). */
-#define HAVE_LIBM
+#define HAVE_LIBM 1
 
 /* Define to 1 if you have the `z' library (-lz). */
-#define HAVE_LIBZ
+#define HAVE_LIBZ 1
 
 /* Define to 1 if you have the <malloc.h> header file. */
-#define HAVE_MALLOC_H
+#define HAVE_MALLOC_H 1
 
 /* Define to 1 if you have the <memory.h> header file. */
 #undef HAVE_MEMORY_H
 
 /* Define to 1 if you have the `memset' function. */
-#define HAVE_MEMSET
+#define HAVE_MEMSET 1
 
 /* Define to 1 if you have the `pow' function. */
 #undef HAVE_POW
 
 /* Define to 1 if you have the <stdint.h> header file. */
-#define HAVE_STDINT_H
+#define HAVE_STDINT_H 1
 
 /* Define to 1 if you have the <stdlib.h> header file. */
-#define HAVE_STDLIB_H
+#define HAVE_STDLIB_H 1
 
 /* Define to 1 if you have the <strings.h> header file. */
 #undef HAVE_STRINGS_H
 
 /* Define to 1 if you have the <string.h> header file. */
-#define HAVE_STRING_H
+#define HAVE_STRING_H 1
 
 /* Define to 1 if you have the <sys/stat.h> header file. */
-#define HAVE_SYS_STAT_H
+#define HAVE_SYS_STAT_H 1
 
 /* Define to 1 if you have the <sys/types.h> header file. */
-#define HAVE_SYS_TYPES_H
+#define HAVE_SYS_TYPES_H 1
 
 /* Define to 1 if you have the <unistd.h> header file. */
-#define HAVE_UNISTD_H
+#define HAVE_UNISTD_H 1
 
 /* Define to the sub-directory in which libtool stores uninstalled libraries.
    */
@@ -59,7 +59,7 @@
 #define PACKAGE_NAME "libpng"
 
 /* Define to the full name and version of this package. */
-#undef PACKAGE_STRING
+#define PACKAGE_STRING "libpng 1.4.4"
 
 /* Define to the one symbol short name of this package. */
 #undef PACKAGE_TARNAME
@@ -68,16 +68,16 @@
 #undef PACKAGE_URL
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.4.3"
+#define PACKAGE_VERSION "1.4.4"
 
 /* Define to 1 if you have the ANSI C header files. */
-#undef STDC_HEADERS
+#define STDC_HEADERS 1
 
 /* Define to 1 if your <sys/time.h> declares `struct tm'. */
-#define TM_IN_SYS_TIME
+#define TM_IN_SYS_TIME 1
 
 /* Version number of package */
-#define VERSION 143
+#define VERSION 144
 
 /* Define to empty if `const' does not conform to ANSI C. */
 #undef const
